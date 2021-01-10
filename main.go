3fb041bd79dbeb2000b1edb68c9503f6c95dfdf1
黄小奇 <hxqxiaoqi1990@163.com>
2021-01-10 15:44:48 +0800

    添加命令行获取端口功能

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/cast"
 	"menu/middle"
 	"menu/router"
 )
 
+var (
+	port int
+)
+
+//初始化函数，用于设置端口
+func init() {
+	flag.IntVar(&port, "p", 8080, "set server port")
+}
+
 func main() {
+	//命令行解析工具
+	flag.Parse()
 	//使用默认中间件创建一个gin路由器
 	r := gin.Default()
 	//解决跨域请求中间件
@@ -23,7 +36,7 @@ func main() {
 	router.K8sRouter(r)
 
 	//服务运行端口
-	err := r.Run(":8080")
+	err := r.Run(":" + cast.ToString(port))
 	if err != nil {
 		fmt.Println("服务运行错误")
 	}
