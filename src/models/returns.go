097d1a1bebbee72248d260f7f2606b9d9bed022d
Outsrkem <xhdascnf@126.com>
2021-01-24 10:23:09 +0800

    添加pageinfo结构体

--- src/models/returns.go
+++ src/models/returns.go
@@ -10,3 +10,20 @@ func NewReturns(result interface{}, msg interface{}) map[string]interface{} {
 
 	return returns
 }
+
+// response 中的页码信息
+type pageInfo struct {
+	Page     int `json:"page"`
+	PageNum  int `json:"pageNum"`
+	PageSize int `json:"pageSize"`
+	Total    int `json:"total"`
+}
+
+func NewPageInfo(page, pageSize, pageNum, total int) *pageInfo {
+	return &pageInfo{
+		Page:     page,
+		PageNum:  pageNum,
+		PageSize: pageSize,
+		Total:    total,
+	}
+}
