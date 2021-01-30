3eb7a6cc62d3bd190c6422b99aa81500e8a5e1af
Outsrkem <xhdascnf@126.com>
2021-01-30 17:42:57 +0800

    主机监控

--- src/controllers/resource/hostMonitor.go
+++ src/controllers/resource/hostMonitor.go
@@ -2,12 +2,16 @@ package resource
 
 import (
 	"github.com/gin-gonic/gin"
+	"mana/src/models"
 	"net/http"
 )
 
 func GetHostMonitorInfo(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"code":    "200",
-		"message": "successfully",
-	})
+	// 临时返回
+	msg := models.NewResMessage("200", "successfully")
+	pageInfo := models.NewPageInfo(1, 1, 1, 1)
+	items := make([]map[string]string, 0)
+	res := models.NewResponse(items, pageInfo)
+	returns := models.NewReturns(res, msg)
+	c.JSON(http.StatusOK, returns)
 }
